e177880bd23b9647e0faed8e302aa9814768e2b5
Aflu <mohamedaflah186@gmail.com>
2024-09-02 17:25:27 +0530

    feat: env local added

--- server/src/controllers/property/getAllproperties.ts
+++ server/src/controllers/property/getAllproperties.ts
@@ -6,16 +6,26 @@ export const getAllProperties = async (req: Request, res: Response) => {
   try {
     const { userId, search, sellTypes } = req.query;
     console.log(userId, " =>");
+    console.log(req.query);
 
-    const properties = await propertyModel.find({
+    const query: any = {
       status: "publish",
-      userId: { $ne: userId },
-    });
+      title: { $regex: search, $options: "i" },
+    };
+
+    if (userId && userId !== "") {
+      query.userId = { $ne: userId };
+    }
+
+    const properties = await propertyModel.find(query);
+    
 
     return res
       .status(200)
       .json({ status: true, message: "Success", properties });
   } catch (error: ErrType) {
+    console.log(error);
+
     return res
       .status(500)
       .json({ status: false, message: (error as any).message });
